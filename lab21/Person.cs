799173ed97d603f1dce7109e644a0bc083ec67c3
FM463 <fatimamustafaeva055@gmail.com>
2021-11-20 16:01:27 +0200

    lab21

---
+++ lab21/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab21
+{
+    class Person
+    {
+        private string name;
+
+        public string Name
+        { 
+            get { return name; }
+            set { name = value; }
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Name.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            Person p = obj as Person;
+            if (p == null)
+                return false;
+            else
+                return this.Name.Equals(p.Name);
+        }
+    }
+
+
+}
---
+++ lab21/Program.cs
@@ -0,0 +1,25 @@
+﻿/*1.Напишите набор простейших классов для создания личных исключений, 
+ * возникающих при работе с объектом типа Student (Worker, Pencil, Animal, Cat …, см. л.р. №17, №18).
+ * 3.Напишите главную программу, демонстрирующую обработку ошибок блоками try-catch. 
+ * Объект создайте в свободной памяти. Проверьте выделения памяти новым способом, используя исключение bad_alloc.*/
+
+using System;
+
+namespace lab21
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            /* Person p1 = new Person();
+            p1.Name = "John";
+            Person p2 = null;
+
+            Console.WriteLine("p1 = p2: {0}", p1.Equals(p2));*/
+            //Personcs person = new Personcs();
+            MyClient.Client();
+            Console.ReadLine();
+
+        }
+    }
+}
